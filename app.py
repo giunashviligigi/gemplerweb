245580530a9d5f6dde2e4bc4cc0c3cc8f5450ec8
giunashviligigi <gigi.giunashvili.1@btu.edu.ge>
2023-06-20 14:37:26 +0400

    latest5

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ class MyApp:
 
 if __name__ == '__main__':
     my_app = MyApp()
-    my_app.app.run(debug=False,host='0.0.0.0', port=8080)
+    my_app.app.run(debug=False,host='0.0.0.0', port=3000)
